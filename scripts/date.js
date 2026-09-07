683e8d6585b82d5e6a45c6c20df9e8c528e24a62
pika4ndy <aandrianarivo1@byupathway.edu>
2026-09-07 21:12:13 +0300

    Responsive design & content on homepage

--- scripts/date.js
+++ scripts/date.js
@@ -6,3 +6,14 @@ const today = new Date();
 lastModifiedText.textContent = `Last Modified ${document.lastModified}`;
 
 currentYear.textContent = today.getFullYear();
+
+// Age Display manager
+const ageDisplay = document.getElementById("myAge");
+const birthday = new Date(2008, 9, 7);
+
+const age = today.getFullYear() - birthday.getFullYear();
+
+
+if (ageDisplay) {
+    ageDisplay.textContent = age;
+}
